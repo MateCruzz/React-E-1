9a2da033c29583873f5f6c821addee7c0359b647
Mateo Cruz <mateocruz8844@gmail.com>
2024-12-23 22:42:05 -0300

    Commit 2 a ver si funciona

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -7,15 +7,15 @@ import CartWidget from '../CardWidget/CartWidget';
 const NavBar = () => {
 
   return (
-    <header>
+    <header className='navbar'>
     <h1>Lyo's Shop</h1>
     <nav>
-        <ul>
-            <li>Inicio</li>
-            <li>Nosotros</li>
-            <li>Productos</li>
-        </ul>
-    </nav>
+  <ul>
+    <li><a href="/">Inicio</a></li>
+    <li><a href="/nosotros">Nosotros</a></li>
+    <li><a href="/productos">Productos</a></li>
+  </ul>
+</nav>
 
 <CartWidget/>
 </header>
